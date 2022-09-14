c98c4205662801189b879f9bebbb77ce76b1e358
onokatio <onokatio@gmail.com>
2022-09-15 03:35:06 +0900

    support wasmer 3.0.0-beta

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -1,5 +1,5 @@
 use std::env;
-use wasmer::{Store, Module, Instance, ImportObject, Exports};
+use wasmer::{Store, Module, Instance, Imports, Exports};
 use wasmer_wasi;
 //use anyhow;
 
@@ -12,13 +12,16 @@ fn privacy_out_vec(nums: i32, len: i32) -> i32 {
     return 0;
 }
 
+#[allow(dead_code)]
 fn deny_syscall_2(_: i32, _: i32) -> i32 {
-    println!("deny_syscall");
+    eprintln!("deny_syscall");
     return 0;
 }
+
+#[allow(dead_code)]
 fn deny_syscall_4(_: i32, _: i32, _: i32, _: i32) -> i32 {
-    println!("deny_syscall");
-    return 0;
+    eprintln!("deny_syscall");
+    return 12;
 }
 
 fn help(){
@@ -35,28 +38,37 @@ fn main() -> anyhow::Result<()>{
         return Err(anyhow::anyhow!("wasm file path is required"));
     }
     let path = &args[1];
-    eprintln!("[Runtime] Loading {}", path);
+    eprintln!("[Runtime] Creating environment(store)...");
+    let mut store = Store::default();
+    eprintln!("[Runtime] Loading {}...", path);
     let wasm_bytes = std::fs::read(path)?;
-    let store = Store::default();
     let module = Module::new(&store, wasm_bytes)?;
+    eprintln!("[Runtime] Creating environment(state)...");
     let mut state_builder = wasmer_wasi::WasiState::new("wasi-prog-name");
     let state = state_builder.build()?;
-    let env = wasmer_wasi::WasiEnv::new(state);
-    let wasmer_import_object = wasmer_wasi::generate_import_object_from_env(&store,env,wasmer_wasi::WasiVersion::Snapshot1);
-    let mut wasi_snapshot_preview1 = wasmer_import_object.get_namespace_exports("wasi_snapshot_preview1").unwrap();
+    eprintln!("[Runtime] Creating environment(env)...");
+    let env = wasmer_wasi::WasiFunctionEnv::new(&mut store, wasmer_wasi::WasiEnv::new(state)).env;
+    eprintln!("[Runtime] Loading syscall implemention...");
+    let mut wasix_32v1 = wasmer_wasi::generate_import_object_from_env(&mut store,&env,wasmer_wasi::WasiVersion::Wasix32v1).get_namespace_exports("wasix_32v1").unwrap();
+    let mut wasix_64v1 = wasmer_wasi::generate_import_object_from_env(&mut store,&env,wasmer_wasi::WasiVersion::Wasix64v1).get_namespace_exports("wasix_64v1").unwrap();
+    let mut wasi_snapshot_preview1 = wasmer_wasi::generate_import_object_from_env(&mut store,&env,wasmer_wasi::WasiVersion::Snapshot1).get_namespace_exports("wasi_snapshot_preview1").unwrap();
     //wasi_snapshot_preview1.insert("random_get", wasmer::Function::new_native(&store, deny_syscall_2));
     //wasi_snapshot_preview1.insert("fd_write", wasmer::Function::new_native(&store, deny_syscall_4));
     let mut wasi_dp_preview1 = Exports::new();
-    wasi_dp_preview1.insert("privacy_out_array5", wasmer::Function::new_native(&store, privacy_out_array5));
-    wasi_dp_preview1.insert("privacy_out_vec", wasmer::Function::new_native(&store, privacy_out_vec));
-    let mut import_object = ImportObject::new();
-    import_object.register("wasi_snapshot_preview1", wasi_snapshot_preview1);
-    import_object.register("wasi_dp_preview1", wasi_dp_preview1);
+    wasi_dp_preview1.insert("privacy_out_array5", wasmer::Function::new_typed(&mut store, privacy_out_array5));
+    wasi_dp_preview1.insert("privacy_out_vec", wasmer::Function::new_typed(&mut store, privacy_out_vec));
+    let mut import_object = Imports::new();
+    //import another function
+    //wasi_snapshot_preview1.insert("sock_accept", wasix_32v1.get_function("sock_accept").unwrap().clone());
+    import_object.register_namespace("wasi_snapshot_preview1", wasi_snapshot_preview1);
+    import_object.register_namespace("wasix_64v1", wasix_64v1);
+    import_object.register_namespace("wasix_32v1", wasix_32v1);
+    import_object.register_namespace("wasi_dp_preview1", wasi_dp_preview1);
     eprintln!("[Runtime] Start {}", path);
-    let instance = Instance::new(&module, &import_object)?;
+    let instance = Instance::new(&mut store,&module, &import_object)?;
     match instance.exports.get_function("_start") {
         Ok(f) => {
-            match f.call(&[]) {
+            match f.call(&mut store,&[]) {
                 Ok(_) => {
                     eprintln!("[Runtime] Done {}", path);
                 },
